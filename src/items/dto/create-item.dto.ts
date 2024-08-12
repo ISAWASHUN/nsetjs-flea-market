dd2c56b567c76e65df06c9cce39052df8de4de3d
ISAWASHUN <syun20000616@icloud.com>
2024-08-12 14:34:53 +0900

    validationの導入

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,6 +1,24 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(0)
   price: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
   description: string;
-  status: 'ON_SALE' | 'SOLD_OUT';
 }
