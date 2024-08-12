d98c4fa5e959103d6e5ab69b33727197d8e0c914
ISAWASHUN <syun20000616@icloud.com>
2024-08-12 14:19:11 +0900

    findByIdの作成

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { Items } from './models/items.model';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll() {
+  findAll(): Items[] {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Items {
+    const item: Items = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON_SALE',
+    };
+    return this.itemsService.create(item);
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Items {
+    return this.itemsService.findById(id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Items } from './models/items.model';
 
 @Injectable()
 export class ItemsService {
-  findAll(): string {
-    return 'Get all item';
+  private items: Items[] = [];
+  findAll(): Items[] {
+    return this.items;
+  }
+
+  create(item: Items): Items {
+    this.items.push(item);
+    return item;
+  }
+
+  findById(id: string): Items {
+    return this.items.find((item) => item.id === id);
   }
 }
