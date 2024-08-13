d958fd382fa7ee58c555436fc70174b304357a07
ISAWASHUN <syun20000616@icloud.com>
2024-08-13 23:57:08 +0900

    signup機能の作成

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from '@prisma/client';
+import { CreateUserInput } from './dto/createUserInput';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async SignUp(@Body() CreateUserInput: CreateUserInput): Promise<User> {
+    return await this.authService.createUser(CreateUserInput);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserInput } from './dto/createUserInput';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(CreateUserInput: CreateUserInput): Promise<User> {
+    const { name, email, password, status } = CreateUserInput;
+
+    return await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password,
+        status,
+      },
+    });
+  }
+}
