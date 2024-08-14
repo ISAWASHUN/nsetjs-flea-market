0997a778141593b7e499f2c74c884fd30fedba33
ISAWASHUN <syun20000616@icloud.com>
2024-08-14 16:18:08 +0900

    signIn機能の作成

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from '@prisma/client';
 import { CreateUserInput } from './dto/createUserInput';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,12 @@ export class AuthController {
   @Post('signup')
   async SignUp(@Body() CreateUserInput: CreateUserInput): Promise<User> {
     return await this.authService.createUser(CreateUserInput);
+  }
+
+  @Post('signin')
+  async SignIn(
+    @Body() credentialsDto: CredentialsDto,
+  ): Promise<{ token: string }> {
+    return await this.authService.signIn(credentialsDto);
   }
 }
